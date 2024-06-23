4489593aa8843c37aafdac619567f165c24e2a3f
Админ <macbahodir@gmail.com>
2024-06-23 13:45:55 +0500

    My homework

---
+++ app.js
@@ -0,0 +1,34 @@
+const searchBtn = document.querySelector('.navigator-btn1');
+const runBtn = document.querySelector('.navigator-btn2');
+const modalClose = document.querySelector('.modal-close');
+const menuModal = document.querySelector('.modal');
+const modalBg = document.querySelector('.modal-bg')
+const text = document.querySelector('.modal__text');
+
+searchBtn.addEventListener('click', function() {
+    menuModal.classList.add('open');
+    modalBg.classList.add('open');
+    text.innerText = "Whoa! You found me!"
+    modalClose.innerText = "Hide Back";
+})
+
+runBtn.addEventListener('click', function() {
+    menuModal.classList.add('open')
+    text.innerText = "Please don't run!"
+    modalClose.innerText = "Close";
+    modalBg.classList.add('open');
+})
+
+modalClose.addEventListener('click', function () {
+    if(menuModal.classList.contains('open')) {
+        menuModal.classList.remove('open')
+    }
+    modalBg.classList.remove('open')
+})
+
+modalBg.addEventListener('click', function () {
+    if(menuModal.classList.contains('open')) {
+        menuModal.classList.remove('open')
+    }
+    modalBg.classList.remove('open')
+})
